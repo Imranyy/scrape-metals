ed9af864fffc3314183814267df9e29c924e9f5e
imranyy <imranmatano6@gmail.com>
2022-05-23 17:25:09 +0300

    initializing scrape

--- js/init.js
+++ js/init.js
@@ -3,4 +3,20 @@ document.addEventListener('DOMContentLoaded', function(){
     M.Sidenav.init(menu,{edge:'right'});
     const modal=document.querySelectorAll('.modal');
     M.Modal.init(modal);
+    //select
+    const select=document.querySelectorAll('select');
+    M.FormSelect.init(select)
 })
+//setupUI for logged in and logged out users
+const loggedinLink=document.querySelectorAll('.log-in')
+const loggedoutLink=document.querySelectorAll('.log-out')
+
+export const setupUI=(user)=>{
+  if(user){
+    loggedinLink.forEach(item=>item.style.display='block')
+    loggedoutLink.forEach(item=>item.style.display='none')
+  }else{
+    loggedinLink.forEach(item=>item.style.display='none')
+    loggedoutLink.forEach(item=>item.style.display='block')
+  }
+}
